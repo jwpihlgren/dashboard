b408d714030d35ae8006841728b5959939504a8e
Joachim Pihlgren <j.w.pihlgren@gmail.com>
2022-08-22 19:20:46 +0200

    Style input field

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { faTimes, faSearch} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search',
@@ -13,20 +14,26 @@ export class SearchComponent implements OnInit, AfterViewInit {
   @ViewChild("search") elementRef!: ElementRef
   searchQuery: string = ""
   searchHasFocus: boolean = false
+  searchIsPristine: boolean = true
+  faTimes = faTimes
+  faSearch = faSearch
   
   ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
-    console.log(this.elementRef.nativeElement); 
+    console.log(this.elementRef.nativeElement)
   }
 
   updateSearchQuery(event: any): void {
     this.searchQuery = event.target.value
+    this.searchIsPristine = this.searchQuery === "" ? true : false
+    console.log(this.searchIsPristine);
   }
 
   clearSearchQuery(): void {
     this.searchQuery = ""
+    this.searchIsPristine = true
     this.elementRef.nativeElement.value = this.searchQuery
     this.elementRef.nativeElement.focus()
   }
