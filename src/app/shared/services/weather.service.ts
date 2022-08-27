7765aa7c5536e1e0ed713dba75d51babe0fe9835
Joachim Pihlgren <j.w.pihlgren@gmail.com>
2022-08-27 12:15:26 +0200

    Add session storage to forecasts

--- src/app/shared/services/weather.service.ts
+++ src/app/shared/services/weather.service.ts
@@ -1,3 +1,4 @@
+import { SessionStorageService } from './session-storage.service';
 import { ILocation } from './../models/location.interface';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -10,19 +11,32 @@ import * as dummydata from '../../../assets/stubs/weather-data.json'
 })
 export class WeatherService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private sessionStorageService:SessionStorageService
+    ) { }
 
   getForecast(location: ILocation){
-    
+
     const url = environment.dev.serverUrl
     const path = `/weather`
     const params = `?lat=${location.lat}&lon=${location.lon}`
+    const hoursUntilExpire = 2
+    const safeName = location.name.replace(" ", "")
+
+    const previousForecasts: any = this.sessionStorageService.getStoredData("forecasts")
+    if(previousForecasts && previousForecasts[safeName] && !this.isExpired(previousForecasts[safeName].expireDate)) {
+        return of(previousForecasts[safeName])
+      }
 
     return this.http.get(`${url}${path}${params}`).pipe(
       map((data:any) => {
         data.location = location.local_name
-        data.fetchDate = Date.now()
-        console.log(data);
+        data.fetchDate = new Date()
+        data.expireDate = new Date(new Date().setHours(data.fetchDate.getHours() + hoursUntilExpire))
+        const previousForecasts: any = this.sessionStorageService.getStoredData("forecasts")
+        previousForecasts[safeName] = data
+        this.sessionStorageService.setStoredData("forecasts", previousForecasts)
         return data
       })
     );
@@ -30,6 +44,10 @@ export class WeatherService {
 
   getIconUrl(icon: string) {
     return `https://openweathermap.org/img/wn/${icon}.png`
+  }
+
+  isExpired(expireDate: any): boolean {
+    return expireDate <= new Date().toISOString()
   }
   
 /*   getForecast(location: any){
