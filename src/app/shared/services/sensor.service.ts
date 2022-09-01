7e98192e2027733e749efa3e004b86d4e543954b
Joachim Pihlgren <j.w.pihlgren@gmail.com>
2022-09-01 21:20:54 +0200

    reactor soilmoisture to calculate averages based on selected period. change styling on ternary button

--- src/app/shared/services/sensor.service.ts
+++ src/app/shared/services/sensor.service.ts
@@ -1,10 +1,10 @@
-
 import { AuthService } from '@auth0/auth0-angular';
 import { HttpClient } from '@angular/common/http';
 import { Injectable, NgZone } from '@angular/core';
 import { map, Subject, Observable, tap, filter, catchError, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { EventSourcePolyfill } from 'event-source-polyfill';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -60,22 +60,23 @@ export class SensorService {
       ))
   }
 
-  getDetailedSensor(id: string, pageSize?: number, pageCount?: number): Observable<any> {
-    const pSize = pageSize ? `pageSize=${pageSize}` : ''
-    const pCount = pageCount ? `pageCount=${pageCount}` : ''
-    const and = pageSize && pageCount ? '&': ''
-    const queryParams = pageSize || pageCount  ? `?${pSize}${and}${pCount}` : ''
-    return this.http.get(`${environment.dev.serverUrl}/sensors/${id}/detailed${queryParams}`)
-  }
-
-  getPeriod(id: string, minDate: Date, maxDate: Date): Observable<any> {
+  getDetailedSensor(id: string, minDate: Date, maxDate: Date, groupBy: ("day" | "hour")): Observable<any> {
     const api = environment.dev.serverUrl
     const path = `/sensors/${id}/period`
     const params = `?minDate=${minDate}&maxDate=${maxDate}`
     return this.http.get(`${api}${path}${params}`).pipe(
-      map((data: any) => data.response)
+      map(
+        (data: any) => {
+          switch(groupBy) {
+            case "day": data.response[0].values = this.groupByDay(data.response[0].values); break; 
+            case "hour": data.response[0].values = this.groupByHour(data.response[0].values); break; 
+          }
+          return data.response
+      })
     )
   }
+
+
 
   getToken(): Observable<any> {
     return this.authService.getAccessTokenSilently()
@@ -87,5 +88,57 @@ export class SensorService {
         'Authorization': `Bearer ${token}`
       }
     });
+  }
+
+  groupByDay(values: any[]): any {
+    const days: any = {}
+    values.forEach((value: any) => {
+      const day = new Date(value.date).toDateString()
+      if(days[day]) days[day].push(value.value)
+      else days[day] = [value.value]
+    })
+
+    return this.calculateAverageForEachEntry(days, "groupByDay")
+  }
+
+  groupByHour(values: any[]): any {
+    const hours: any = {}
+    values.forEach((value: any) => {
+      const hour: number = new Date(value.date).getHours()
+      if(hours[hour]) hours[hour].push(value.value)
+      else hours[hour] = [value.value]
+    })
+    return this.calculateAverageForEachEntry(hours, "groupByHour")
+  }
+
+  private calculateAverageForEachEntry(obj: any, caller: "groupByDay" | "groupByHour"): any {
+    const result: any = []
+    for (const [key, values] of Object.entries(obj)) {
+      const typedKey: string = key as string
+      const typedValues: number[] = values as number[]
+      const average: number = this.getAverage(typedValues)
+
+      let date
+      /* If groupByDay, typedKey is a date, else it's a string number representing the hour */
+      if(caller === "groupByDay") {
+        date = new Date(typedKey)
+      }
+      else {
+        date = new Date()
+        date.setHours(parseInt(typedKey))
+      }
+      result.push(
+        {
+          value: average,
+          date: date
+        }
+      )
+    }
+    return result
+
+  }
+  
+  private getAverage(arr: number[]): number {
+    return Math.round(arr.reduce((acc: number, cur: number) => acc + cur) / arr.length)
   }
 }
