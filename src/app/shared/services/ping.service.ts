bf1281ba1b1e46c71373fe15305b606dd7425296
Joachim Pihlgren <j.pihlgren@outlook.com>
2023-05-03 18:35:12 +0200

    Add ping service, fix bug in bar chart

---
+++ src/app/shared/services/ping.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { EMPTY, Observable, catchError, concat, delayWhen, interval, retry, retryWhen, switchMap, take, tap, throwError, timeout, timer } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PingService {
+
+  constructor(private http: HttpClient) { }
+
+
+  ping(): Observable<boolean> {
+
+    const url = environment.dev.serverUrl
+    const path = `/ping`
+    const delay = 60 * 1000
+    const timesToRetry = 3
+    const interval = 1 * 60 * 1000
+    return timer(0, interval).pipe(
+      switchMap(() => {
+        return this.http.get<boolean>(`${url}${path}`).pipe(
+          retryWhen(errors => errors.pipe(
+            delayWhen(() => timer(delay)),
+            take(timesToRetry),
+            tap(() => console.log(`Time out after retrying`))
+            
+          )),
+          catchError(error => {
+            console.log(error);
+            return EMPTY
+          })
+        )
+      })
+    )
+  }
+}
