74306af6d6d793315bf120047ea6e8adef36fccb
Joachim Pihlgren <j.pihlgren@outlook.com>
2023-06-01 13:03:47 +0200

    Display read-only name on datailed sensor for identification

--- src/app/shared/models/sensor.interface.ts
+++ src/app/shared/models/sensor.interface.ts
@@ -1,8 +1,7 @@
-import { ISoilMoistureData } from "./soil-moisture-data.interface"
-
 export interface ISensor {
-    _id: string,
-    alias: string,
-    createdAt: Date,
+    _id: string
+    alias: string
+    name: string
+    createdAt: Date
     updatedAt: Date
 }
