bd5ffee94386796466baad9111285053630e7b8e
Joachim Pihlgren <j.pihlgren@outlook.com>
2023-09-23 15:57:45 +0200

    Check to see if char count is within allowed before setting item in local storage

--- src/app/shared/services/local-storage.service.ts
+++ src/app/shared/services/local-storage.service.ts
@@ -5,14 +5,21 @@ import { Injectable } from '@angular/core';
 })
 export class LocalStorageService {
 
+  MAX_STORAGE_SIZE = 5242880
+
   constructor() { }
 
   getStoredData(key: string): object {
     return JSON.parse(localStorage.getItem(key) || "{}")
   }
 
-  setStoredData(key: string, data: object): void {
+   setStoredData(key: string, data: object): void {
+    console.log(JSON.stringify(data).length);
+    if(JSON.stringify(data).length >= this.MAX_STORAGE_SIZE) return
+
     localStorage.setItem(key, JSON.stringify(data))
+
+    
   }
 
   deleteStoredData(key: string): void {
